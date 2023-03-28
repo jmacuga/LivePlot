cac329b6c423a5c43afc1380d0f93a9e2db830dc
jmacuga <julia.macuga@gmail.com>
2023-03-29 00:02:38 +0200

    formating changes

--- plot_generating3.py
+++ plot_generating3.py
@@ -8,15 +8,22 @@ import pandas as pd
 
 NUMBER_OF_SENSORS = 3
 
+
 class serialPlot:
 
-    def __init__(self, serialPort='/dev/ttyACM0', serialBaud=115200, plotLength=100, dataNumBytes=NUMBER_OF_SENSORS):
+    def __init__(self,
+                 serialPort='/dev/ttyACM0',
+                 serialBaud=115200,
+                 plotLength=100,
+                 dataNumBytes=NUMBER_OF_SENSORS):
         self.port = serialPort
         self.baud = serialBaud
         self.plotMaxLength = plotLength
         self.dataNumBytes = dataNumBytes
-        self.rawData = ''
-        self.data_buffers = [collections.deque([0] * plotLength, maxlen=plotLength) for channel_num in range(NUMBER_OF_SENSORS)]
+        self.rawData = []
+        self.data_buffers = [collections.deque(
+            [0] * plotLength, maxlen=plotLength)
+            for _ in range(NUMBER_OF_SENSORS)]
         self.isRun = True
         self.isReceiving = False
         self.thread = None
@@ -41,23 +48,27 @@ class serialPlot:
             self.thread = Thread(target=self.backgroundThread)
             self.thread.start()
             # Block till we start receiving values
-            while self.isReceiving != True:
+            while self.isReceiving is not True:
                 time.sleep(0.1)
 
-    def getSerialData(self, frame, lines, lineValueTexts, lineLabels, timeText):
+    def plot_serial_data(self,
+                         frame,
+                         lines,
+                         lineValueTexts,
+                         lineLabels,
+                         timeText):
         currentTimer = time.perf_counter()
-        # the first reading will be erroneous
         self.plotTimer = int((currentTimer - self.previousTimer) * 1000)
         self.previousTimer = currentTimer
-        timeText.set_text('Plot Interval = ' + str(self.plotTimer) + 'ms')     
-        # we get the latest data point and append it to our array
-        # plot_readings(lines, lineValueTexts, lineLabels, timeText)
+        timeText.set_text('Plot Interval = ' + str(self.plotTimer) + 'ms')
+        # self.convert_readings()
         # plot_sum()
-        for count, [buffer, line, lineValueText] in enumerate(zip(self.data_buffers, lines, lineValueTexts)):
+        for count, [buffer, line, lineValueText] in enumerate(
+                zip(self.data_buffers, lines, lineValueTexts)):
             buffer.append(self.rawData[count])
             line.set_data(range(self.plotMaxLength), self.data_buffers[count])
-            lineValueText.set_text('[' + lineLabels[count] + '] = ' + str(self.rawData[count]))
-            
+            lineValueText.set_text(
+                '[' + lineLabels[count] + '] = ' + str(self.rawData[count]))
         # self.csvData.append(self.data1[-1])
 
     def backgroundThread(self):    # retrieve data
@@ -68,7 +79,8 @@ class serialPlot:
             line.strip()
             data = line.split(',')
             try:
-                self.rawData = [float(data[i]) for i in range(NUMBER_OF_SENSORS)]
+                self.rawData = [float(data[i])
+                                for i in range(NUMBER_OF_SENSORS)]
             except TypeError:
                 self.rawData = [0.0 for _ in range(NUMBER_OF_SENSORS)]
             except ValueError:
@@ -91,7 +103,7 @@ def main():
     portName = '/dev/ttyACM0'
     baudRate = 115200
     maxPlotLength = 100
-    dataNumBytes = 4  
+    dataNumBytes = 4
     # number of bytes of 1 data point
     # initializes all required variables
     s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes)
@@ -105,7 +117,7 @@ def main():
     ymin = -(1)
     ymax = 3.3
     fig = plt.figure()
-    ax = plt.axes(xlim=(xmin, xmax), ylim=( 
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(
         float(ymin - (ymax - ymin) / 10), float(ymax + (ymax - ymin) / 10)))
     ax.set_title('STM32 Analog Read')
     ax.set_xlabel("time")
@@ -113,11 +125,16 @@ def main():
 
     labels = [f'Channel {sen_num + 1}' for sen_num in range(NUMBER_OF_SENSORS)]
     timeText = ax.text(0.50, 0.95, '', transform=ax.transAxes)
-    lines = [ax.plot([], [], label=labels[sen_num])[0] for sen_num in range(NUMBER_OF_SENSORS)]
+    lines = [ax.plot([], [], label=labels[sen_num])[0]
+             for sen_num in range(NUMBER_OF_SENSORS)]
     offset = 0.05
-    lineValueTexts = [ax.text(0.50, 0.90 - offset * sen_num, '', transform=ax.transAxes) for sen_num in range(NUMBER_OF_SENSORS) ]
+    lineValueTexts = [ax.text(0.50, 0.90 - offset * sen_num,
+                              '',
+                              transform=ax.transAxes)
+                      for sen_num in range(NUMBER_OF_SENSORS)]
 
-    anim = animation.FuncAnimation(fig, s.getSerialData, fargs=(lines, lineValueTexts, labels, timeText), interval=pltInterval)    # fargs has to be a tuple
+    _ = animation.FuncAnimation(fig, s.plot_serial_data, fargs=(
+        lines, lineValueTexts, labels, timeText), interval=pltInterval)
     plt.legend(loc="upper left")
     plt.show()
     s.close()
